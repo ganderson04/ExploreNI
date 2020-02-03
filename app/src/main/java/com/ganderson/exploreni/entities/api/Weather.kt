83e32dc73f642aba3d3756804ed76b5f32e8e50b
George Anderson <gjanderson04@gmail.com>
2020-02-03 20:50:40 +0000

    Removed documentation for updated Weather class.

--- app/src/main/java/com/ganderson/exploreni/entities/api/Weather.kt
+++ app/src/main/java/com/ganderson/exploreni/entities/api/Weather.kt
@@ -1,8 +1,3 @@
 package com.ganderson.exploreni.entities.api
 
-/**
- * Represents the parts of the OpenWeatherMap API response that are used for this app. Through the
- * use of the "SerializedName" annotation, Retrofit can map the fields of the JSON response to the
- * fields of the class.
- */
 data class Weather(val desc: String, val temp: Double)
