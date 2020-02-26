f6762dc8a1b29fd60e756afc43440c8164f7402e
George Anderson <gjanderson04@gmail.com>
2020-02-26 23:29:57 +0000

    Removed goBack() and inner adapter from Event code.

--- app/src/main/java/com/ganderson/exploreni/ui/fragments/EventFragment.kt
+++ app/src/main/java/com/ganderson/exploreni/ui/fragments/EventFragment.kt
@@ -79,49 +79,4 @@ class EventFragment : Fragment() {
         }
         return super.onOptionsItemSelected(item)
     }
-
-    private fun goBack() {
-        val mainActivity = this.activity as MainActivity
-        mainActivity.supportFragmentManager.popBackStack()
-    }
-
-//    class EventAdapter(private val events: List<Event>)
-//        : RecyclerView.Adapter<EventAdapter.EventViewHolder>() {
-//        private val eventDateFormatter = DateTimeFormatter.ofPattern("E d MMM")
-//
-//        class EventViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
-//            val cvEvent = view.findViewById<CardView>(R.id.cvEvent)
-//            val ivEvent = view.findViewById<ImageView>(R.id.ivEvent)
-//            val tvEventName = view.findViewById<TextView>(R.id.tvEventName)
-//            val tvEventDates = view.findViewById<TextView>(R.id.tvEventDates)
-//        }
-//
-//        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EventViewHolder {
-//            val view = LayoutInflater.from(parent.context)
-//                .inflate(R.layout.layout_event_item, parent, false)
-//            return EventViewHolder(view)
-//        }
-//
-//        override fun onBindViewHolder(holder: EventViewHolder, position: Int) {
-//            val event = events[position]
-//
-//            Glide.with(holder.view.context)
-//                .load(event.imgUrl)
-//                .centerCrop()
-//                .error(R.drawable.placeholder_no_image_available)
-//                .into(holder.ivEvent)
-//
-//            val dateString = "${event.startDate.format(eventDateFormatter)} - ${event.endDate.format(eventDateFormatter)}"
-//
-//            holder.tvEventName.text = event.name
-//            holder.tvEventDates.text = dateString
-//            holder.cvEvent.setOnClickListener {
-//                val eventDetailFragment = EventDetailFragment(event)
-//                val mainActivity = holder.view.context as MainActivity
-//                mainActivity.displayFragment(eventDetailFragment)
-//            }
-//        }
-//
-//        override fun getItemCount() = events.size
-//    }
 }
