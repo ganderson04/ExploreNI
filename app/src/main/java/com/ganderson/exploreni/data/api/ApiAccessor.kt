6b3309227821755a2c1a94a9baf09509415bdeb2
George Anderson <gjanderson04@gmail.com>
2020-03-18 11:02:59 +0000

    Separated API service creation and usage.

--- app/src/main/java/com/ganderson/exploreni/data/api/ApiAccessor.kt
+++ app/src/main/java/com/ganderson/exploreni/data/api/ApiAccessor.kt
@@ -2,32 +2,23 @@ package com.ganderson.exploreni.data.api
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import com.ganderson.exploreni.data.api.services.ExploreService
 import com.ganderson.exploreni.data.api.services.GeocodingService
-import com.ganderson.exploreni.data.api.services.WeatherService
 import com.ganderson.exploreni.entities.api.Event
 import com.ganderson.exploreni.entities.api.NiLocation
 import com.ganderson.exploreni.entities.api.Weather
-import com.google.gson.GsonBuilder
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 class ApiAccessor {
     companion object {
         private const val API_ERROR_CODE = 404
-        private val exploreLocationService by lazy { constructExploreLocationService() }
-        private val exploreEventService by lazy { constructExploreEventService() }
-        private val geocodingService by lazy { constructGeocodingService() }
-        private val weatherService by lazy { constructWeatherService() }
 
         fun getNearbyLocations(lat: Double, lon: Double, radius:Int)
                 : LiveData<List<NiLocation>> {
             val data = MutableLiveData<List<NiLocation>>()
 
-            val call = exploreLocationService.getNearbyLocations(lat, lon, radius)
+            val call = ApiServices.exploreLocationService.getNearbyLocations(lat, lon, radius)
             call.enqueue(object: Callback<List<NiLocation>> {
                 override fun onFailure(call: Call<List<NiLocation>>, t: Throwable) {
                     throw t
@@ -50,7 +41,7 @@ class ApiAccessor {
         fun getLocationsByType(type: String) : LiveData<List<NiLocation>> {
             val data = MutableLiveData<List<NiLocation>>()
 
-            val call = exploreLocationService.getLocationsByType(type)
+            val call = ApiServices.exploreLocationService.getLocationsByType(type)
             call.enqueue(object: Callback<List<NiLocation>> {
                 override fun onFailure(call: Call<List<NiLocation>>, t: Throwable) {
                     throw t
@@ -73,7 +64,7 @@ class ApiAccessor {
         fun getEvents() : LiveData<List<Event>> {
             val data = MutableLiveData<List<Event>>()
 
-            val call = exploreEventService.getEvents()
+            val call = ApiServices.exploreEventService.getEvents()
             call.enqueue(object: Callback<List<Event>> {
                 override fun onFailure(call: Call<List<Event>>, t: Throwable) {
                     throw t
@@ -96,7 +87,7 @@ class ApiAccessor {
         fun performSearch(query: String) : LiveData<List<NiLocation>> {
             val data = MutableLiveData<List<NiLocation>>()
 
-            val call = exploreLocationService.performSearch(query)
+            val call = ApiServices.exploreLocationService.performSearch(query)
             call.enqueue(object: Callback<List<NiLocation>> {
                 override fun onFailure(call: Call<List<NiLocation>>, t: Throwable) {
                     throw t
@@ -124,7 +115,7 @@ class ApiAccessor {
             geocodingData["result_type"] = GeocodingService.RESULT_TYPE
             geocodingData["key"] = apiKey
 
-            val geocodingCall = geocodingService.reverseGeocode(geocodingData)
+            val geocodingCall = ApiServices.geocodingService.reverseGeocode(geocodingData)
             geocodingCall.enqueue(object : Callback<String> {
                 override fun onFailure(call: Call<String>, t: Throwable) {
                     throw t
@@ -155,7 +146,7 @@ class ApiAccessor {
             weatherData["APPID"] = apiKey
 
             // Make, enqueue and process the call.
-            val weatherCall = weatherService.getCurrentWeather(weatherData)
+            val weatherCall = ApiServices.weatherService.getCurrentWeather(weatherData)
             weatherCall.enqueue(object : Callback<Weather> {
                 override fun onFailure(call: Call<Weather>, t: Throwable) {
                     throw t
@@ -168,54 +159,6 @@ class ApiAccessor {
             })
 
             return data
-        }
-
-        private fun constructExploreLocationService() : ExploreService {
-            val locationDeserialiser = GsonBuilder()
-                .registerTypeAdapter(NiLocation::class.java, ExploreService.LocationDeserialiser())
-                .create()
-
-            return Retrofit.Builder()
-                .baseUrl(ExploreService.BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create(locationDeserialiser))
-                .build()
-                .create(ExploreService::class.java)
-        }
-
-        private fun constructExploreEventService() : ExploreService {
-            val eventDeserialiser = GsonBuilder()
-                .registerTypeAdapter(Event::class.java, ExploreService.EventDeserialiser())
-                .create()
-
-            return Retrofit.Builder()
-                .baseUrl(ExploreService.BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create(eventDeserialiser))
-                .build()
-                .create(ExploreService::class.java)
-        }
-
-        private fun constructGeocodingService() : GeocodingService {
-            val geocodingDeserialiser = GsonBuilder()
-                .registerTypeAdapter(String::class.java, GeocodingService.GeocodingDeserialiser())
-                .create()
-
-            return Retrofit.Builder()
-                .baseUrl(GeocodingService.BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create(geocodingDeserialiser))
-                .build()
-                .create(GeocodingService::class.java)
-        }
-
-        private fun constructWeatherService() : WeatherService {
-            val weatherDeserialiser = GsonBuilder()
-                .registerTypeAdapter(Weather::class.java, WeatherService.WeatherDeserialiser())
-                .create()
-
-            return Retrofit.Builder()
-                .baseUrl(WeatherService.BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create(weatherDeserialiser))
-                .build()
-                .create(WeatherService::class.java)
         }
     }
 }
---
+++ app/src/main/java/com/ganderson/exploreni/data/api/ApiServices.kt
@@ -0,0 +1,68 @@
+package com.ganderson.exploreni.data.api
+
+import com.ganderson.exploreni.data.api.services.ExploreService
+import com.ganderson.exploreni.data.api.services.GeocodingService
+import com.ganderson.exploreni.data.api.services.WeatherService
+import com.ganderson.exploreni.entities.api.Event
+import com.ganderson.exploreni.entities.api.NiLocation
+import com.ganderson.exploreni.entities.api.Weather
+import com.google.gson.GsonBuilder
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class ApiServices {
+    companion object {
+        val exploreLocationService by lazy { constructExploreLocationService() }
+        val exploreEventService by lazy { constructExploreEventService() }
+        val geocodingService by lazy { constructGeocodingService() }
+        val weatherService by lazy { constructWeatherService() }
+
+        private fun constructExploreLocationService() : ExploreService {
+            val locationDeserialiser = GsonBuilder()
+                .registerTypeAdapter(NiLocation::class.java, ExploreService.LocationDeserialiser())
+                .create()
+
+            return Retrofit.Builder()
+                .baseUrl(ExploreService.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(locationDeserialiser))
+                .build()
+                .create(ExploreService::class.java)
+        }
+
+        private fun constructExploreEventService() : ExploreService {
+            val eventDeserialiser = GsonBuilder()
+                .registerTypeAdapter(Event::class.java, ExploreService.EventDeserialiser())
+                .create()
+
+            return Retrofit.Builder()
+                .baseUrl(ExploreService.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(eventDeserialiser))
+                .build()
+                .create(ExploreService::class.java)
+        }
+
+        private fun constructGeocodingService() : GeocodingService {
+            val geocodingDeserialiser = GsonBuilder()
+                .registerTypeAdapter(String::class.java, GeocodingService.GeocodingDeserialiser())
+                .create()
+
+            return Retrofit.Builder()
+                .baseUrl(GeocodingService.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(geocodingDeserialiser))
+                .build()
+                .create(GeocodingService::class.java)
+        }
+
+        private fun constructWeatherService() : WeatherService {
+            val weatherDeserialiser = GsonBuilder()
+                .registerTypeAdapter(Weather::class.java, WeatherService.WeatherDeserialiser())
+                .create()
+
+            return Retrofit.Builder()
+                .baseUrl(WeatherService.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(weatherDeserialiser))
+                .build()
+                .create(WeatherService::class.java)
+        }
+    }
+}
