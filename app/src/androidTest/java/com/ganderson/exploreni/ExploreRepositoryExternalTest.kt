b402b7f77ecb1f0603951d07a3d1039572d23949
George Anderson <gjanderson04@gmail.com>
2020-04-06 18:08:57 +0100

    Added some tests.

---
+++ app/src/androidTest/java/com/ganderson/exploreni/ExploreRepositoryExternalTest.kt
@@ -0,0 +1,93 @@
+package com.ganderson.exploreni
+
+import android.location.Location
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.LiveData
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
+import com.ganderson.exploreni.data.ExploreRepository
+import com.ganderson.exploreni.data.api.ApiAccessor
+import com.ganderson.exploreni.entities.Itinerary
+import com.ganderson.exploreni.entities.api.NiLocation
+import com.ganderson.exploreni.entities.api.Weather
+import com.ganderson.exploreni.ui.activities.MainActivity
+import com.jraska.livedata.test
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class ExploreRepositoryExternalTest {
+    private lateinit var location: Location
+    private lateinit var itinerary: Itinerary
+    private lateinit var openWeatherMapApiKey: String
+    private lateinit var googleApiKey: String
+
+    // Used to execute background tasks synchronously.
+    @get:Rule val taskRule = InstantTaskExecutorRule()
+    @get:Rule val activityRule = ActivityTestRule(MainActivity::class.java)
+
+    @Before
+    fun constructLocation() {
+        location = Location("")
+        location.latitude = 54.597181
+        location.longitude = -5.930148
+    }
+
+    @Before
+    fun constructItinerary() {
+        val dbId = ""
+        val name = ""
+        val niLocation = NiLocation("", "", 0f, "", "54.642748",
+            "-5.942263", "", "", "", ArrayList())
+        val locationList = ArrayList<NiLocation>().apply {
+            add(niLocation)
+        }
+        itinerary = Itinerary("", "", locationList)
+    }
+
+    @Before
+    fun getApiKeys() {
+        openWeatherMapApiKey = activityRule.activity.getString(R.string.openweathermap_api_key)
+        googleApiKey = activityRule.activity.getString(R.string.google_api_key)
+    }
+
+    @Test
+    fun getWeatherCelsiusTest() {
+        val useFahrenheit = false
+        ExploreRepository
+            .getWeather(location.latitude, location.longitude, useFahrenheit, openWeatherMapApiKey)
+            .test()
+            .awaitValue()
+            .assertHasValue()
+    }
+
+    @Test
+    fun getWeatherFahrenheitTest() {
+        val useFahrenheit = true
+        ExploreRepository
+            .getWeather(location.latitude, location.longitude, useFahrenheit, openWeatherMapApiKey)
+            .test()
+            .awaitValue()
+            .assertHasValue()
+    }
+
+    @Test
+    fun calculateDurationTest() {
+        ExploreRepository
+            .calculateDuration(itinerary, location, googleApiKey)
+            .test()
+            .awaitValue()
+            .assertHasValue()
+    }
+
+    @Test
+    fun getItineraryPolylineTest() {
+        ExploreRepository
+            .getItineraryPolyline(itinerary, location, googleApiKey)
+            .test()
+            .awaitValue()
+            .assertHasValue()
+    }
+}
---
+++ app/src/test/java/com/ganderson/exploreni/ExploreRepositoryHerokuTest.kt
@@ -0,0 +1,80 @@
+package com.ganderson.exploreni
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.ganderson.exploreni.data.ExploreRepository
+import com.ganderson.exploreni.entities.LocationType
+import com.jraska.livedata.test
+import org.junit.Rule
+import org.junit.Test
+
+class ExploreRepositoryHerokuTest {
+    private val latitude = 54.597181
+    private val longitude = -5.930148
+    private val radius = 5 // miles
+
+    // Used to execute background tasks synchronously.
+    @get:Rule val taskRule = InstantTaskExecutorRule()
+
+    @Test
+    fun getNearbyLocationsTest() {
+        ExploreRepository
+            .getNearbyLocations(latitude, longitude, radius)
+            .test()
+            .awaitValue()
+            .assertHasValue()
+    }
+
+    @Test
+    fun getLocationsByTypeSeeTest() {
+        ExploreRepository
+            .getLocationsByType(LocationType.SEE.name)
+            .test()
+            .awaitValue()
+            .assertHasValue()
+    }
+
+    @Test
+    fun getLocationsByTypeDoTest() {
+        ExploreRepository
+            .getLocationsByType(LocationType.DO.name)
+            .test()
+            .awaitValue()
+            .assertHasValue()
+    }
+
+    @Test
+    fun getLocationsByTypeStayTest() {
+        ExploreRepository
+            .getLocationsByType(LocationType.STAY.name)
+            .test()
+            .awaitValue()
+            .assertHasValue()
+    }
+
+    @Test
+    fun getLocationsByTypeEatTest() {
+        ExploreRepository
+            .getLocationsByType(LocationType.EAT.name)
+            .test()
+            .awaitValue()
+            .assertHasValue()
+    }
+
+    @Test
+    fun getEvents() {
+        ExploreRepository
+            .getEvents()
+            .test()
+            .awaitValue()
+            .assertHasValue()
+    }
+
+    @Test
+    fun performSearch() {
+        ExploreRepository
+            .performSearch("Giant's Causeway")
+            .test()
+            .awaitValue()
+            .assertHasValue()
+    }
+}
