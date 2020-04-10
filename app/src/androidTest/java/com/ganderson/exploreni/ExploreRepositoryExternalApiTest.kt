5454f8dfb66902e89dd3c3460a151dffe73bee5d
George Anderson <gjanderson04@gmail.com>
2020-04-10 21:39:06 +0100

    Renamed repository instrumented test class.

--- app/src/androidTest/java/com/ganderson/exploreni/ExploreRepositoryExternalApiTest.kt
+++ app/src/androidTest/java/com/ganderson/exploreni/ExploreRepositoryExternalApiTest.kt
@@ -15,7 +15,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class ExploreRepositoryExternalTest {
+class ExploreRepositoryExternalApiTest {
     private lateinit var location: Location
     private lateinit var itinerary: Itinerary
     private lateinit var openWeatherMapApiKey: String
