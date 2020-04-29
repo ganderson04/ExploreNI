05ab87209250538f3cd9d61a21bb181a4c6be095
George Anderson <gjanderson04@gmail.com>
2020-04-29 21:27:29 +0100

    Separated MyItinerariesAdapter.

--- app/src/main/java/com/ganderson/exploreni/ui/fragments/MyItinerariesFragment.kt
+++ app/src/main/java/com/ganderson/exploreni/ui/fragments/MyItinerariesFragment.kt
@@ -1,25 +1,20 @@
 package com.ganderson.exploreni.ui.fragments
 
 import android.app.AlertDialog
-import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageButton
-import android.widget.TextView
-import android.widget.Toast
-import androidx.cardview.widget.CardView
 import androidx.lifecycle.observe
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.ganderson.exploreni.EspressoIdlingResource
 
 import com.ganderson.exploreni.R
 import com.ganderson.exploreni.entities.Itinerary
 import com.ganderson.exploreni.ui.activities.MainActivity
+import com.ganderson.exploreni.ui.components.adapters.MyItinerariesAdapter
 import com.ganderson.exploreni.ui.viewmodels.MyItinerariesViewModel
 import kotlinx.android.synthetic.main.fragment_my_itineraries.*
 
@@ -81,49 +76,5 @@ class MyItinerariesFragment : Fragment() {
             }
         }
         return super.onOptionsItemSelected(item)
-    }
-
-    class MyItinerariesAdapter(val context: Context, private val itineraryList: List<Itinerary>,
-                               private val listener: OnRemoveClickListener)
-        : RecyclerView.Adapter<MyItinerariesAdapter.ItineraryViewHolder>() {
-
-        interface OnRemoveClickListener {
-            fun onRemoveClick(itinerary: Itinerary)
-        }
-
-        class ItineraryViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
-            val cvMyItinerary = view.findViewById<CardView>(R.id.cvMyItinerary)
-            val tvMyItineraryName = view.findViewById<TextView>(R.id.tvMyItineraryName)
-            val tvMyItinerarySize = view.findViewById<TextView>(R.id.tvMyItinerarySize)
-            val ibRemoveItinerary = view.findViewById<ImageButton>(R.id.ibRemoveItinerary)
-        }
-
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int)
-                : ItineraryViewHolder {
-            val view = LayoutInflater.from(context)
-                .inflate(R.layout.layout_my_itineraries_item, parent, false)
-            return ItineraryViewHolder(view)
-        }
-
-        override fun getItemCount(): Int = itineraryList.size
-
-        override fun onBindViewHolder(holder: ItineraryViewHolder,
-                                      position: Int) {
-            val itinerary = itineraryList[position]
-            holder.tvMyItineraryName.text = itinerary.name
-
-            var sizeText = "${itinerary.itemList.size} location"
-            if(itinerary.itemList.size > 1) sizeText += "s"
-
-            holder.tvMyItinerarySize.text = sizeText
-
-            holder.cvMyItinerary.setOnClickListener {
-                val mainActivity = context as MainActivity
-                mainActivity.displayFragment(ItineraryViewerFragment(false, itinerary))
-            }
-
-            holder.ibRemoveItinerary.setOnClickListener { listener.onRemoveClick(itinerary) }
-        }
-
     }
 }
